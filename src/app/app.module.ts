e3807c01010e8cd8f01790ecfec48cecf37a2b86
Ayush Shashank <ayush.bibhas@gmail.com>
2023-06-10 15:44:18 +0530

    generate insights component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InsightsComponent } from './components/insights/insights.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InsightsComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
