a3c2aac849ac409676fa521fd488eb156f5eed77
Ayush Shashank <ayush.bibhas@gmail.com>
2023-06-11 12:51:28 +0530

    pat based auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gh-analyser';
+  isAuth = false;
+
+  constructor(private gh: GithubService) {}
+
+  async ngOnInit() {
+    const pat = localStorage.getItem('gh-pat');
+    if (pat) {
+      if (!(await this.gh.verifyPAT(pat))) {
+        alert('Token Expired!');
+        this.getPAT();
+      } else {
+        this.isAuth = true;
+        this.repoSetup();
+      }
+    } else {
+      this.getPAT();
+    }
+  }
+
+  async getPAT() {
+    const token = prompt('Enter you Personal Access Token');
+    if (token && (await this.gh.verifyPAT(token))) {
+      localStorage.setItem('gh-pat', token);
+      this.isAuth = true;
+      this.selectRepos();
+    } else {
+      alert('Token Invalid!');
+      this.getPAT();
+    }
+  }
+
+  selectRepos() {}
+  repoSetup() {
+    console.log('respoSetup');
+  }
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,41 @@
+import {
+  HttpClient
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Octokit } from '@octokit/rest';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubService {
+  // baseUrl = `https://api.github.com`;
+  pat = '';
+  isPATValid = false;
+  gh: Octokit | undefined = undefined;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  setAccessToken(token: string) {
+    this.pat = token;
+  }
+
+  initOctokit() {
+    this.gh = new Octokit({
+      auth: this.pat,
+      log: console,
+    });
+  }
+
+  verifyPAT(token: string) {
+    const octo = new Octokit({ auth: token });
+    return octo.users
+      .getAuthenticated()
+      .then((response) => {
+        this.pat = token;
+        this.gh = new Octokit({ auth: token });
+        console.log(response.data.login);
+        return true;
+      })
+      .catch((_) => false);
+  }
 }
