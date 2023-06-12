a2221b9e51d7d3e83b4382b276da7712fb2a9e02
Ayush Shashank <ayush.bibhas@gmail.com>
2023-06-12 21:02:06 +0530

    modules fixup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { InsightsComponent } from './components/insights/insights.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TopContributorByCommitComponent } from './components/insights/top-contributor-by-commit/top-contributor-by-commit.component';
+import { TopContributorByPRComponent } from './components/insights/top-contributor-by-pr/top-contributor-by-pr.component';
+import { CommitFrequencyComponent } from './components/insights/commit-frequency/commit-frequency.component';
+import { ComparisonTableComponent } from './components/insights/comparison-table/comparison-table.component';
 
 @NgModule({
-  declarations: [AppComponent, InsightsComponent, SettingsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, InsightsComponent, SettingsComponent, TopContributorByCommitComponent, TopContributorByPRComponent, CommitFrequencyComponent, ComparisonTableComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbNavModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
