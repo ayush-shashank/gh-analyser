b273fba759c0df6c223d8e2fde1c12c6ca5db1e2
Ayush Shashank <ayush.bibhas@gmail.com>
2023-06-13 02:16:17 +0530

    comparison table update

--- src/app/components/insights/comparison-table/comparison-table.component.ts
+++ src/app/components/insights/comparison-table/comparison-table.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stats } from 'src/app/models/repoStats.interface';
+import { GlobalStateService } from 'src/app/services/global-state.service';
+import { InsightsService } from 'src/app/services/insights.service';
 
 @Component({
   selector: 'app-comparison-table',
   templateUrl: './comparison-table.component.html',
-  styleUrls: ['./comparison-table.component.scss']
+  styleUrls: ['./comparison-table.component.scss'],
 })
-export class ComparisonTableComponent {
+export class ComparisonTableComponent implements OnInit {
+  selectedRepos: { owner: string; name: string; stats?: Stats }[] = [];
+  constructor(
+    private insightsService: InsightsService,
+    private gs: GlobalStateService
+  ) {
+    this.selectedRepos = this.gs.selectedRepos;
+  }
+  ngOnInit() {
+    this.getStats();
+  }
 
+  getStats() {
+    console.log(this.selectedRepos);
+    this.selectedRepos.map(async (repo) => {
+      this.insightsService
+        .getStats(repo.owner, repo.name)
+        .then((stats) => {
+          repo.stats = stats;
+          return repo;
+        })
+        .catch((err) => console.error('GET Stats Failed', err));
+    });
+  }
 }
