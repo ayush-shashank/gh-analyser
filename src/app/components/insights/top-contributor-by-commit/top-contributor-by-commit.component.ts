b6f5c2b9662c772e7eb8a1e3d1537c931d851cb5
Ayush Shashank <ayush.bibhas@gmail.com>
2023-06-14 02:32:11 +0530

    data fix for top contributor by commit

--- src/app/components/insights/top-contributor-by-commit/top-contributor-by-commit.component.ts
+++ src/app/components/insights/top-contributor-by-commit/top-contributor-by-commit.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.interface';
+import { GlobalStateService } from 'src/app/services/global-state.service';
+import { InsightsService } from 'src/app/services/insights.service';
 
 @Component({
   selector: 'app-top-contributor-by-commit',
   templateUrl: './top-contributor-by-commit.component.html',
-  styleUrls: ['./top-contributor-by-commit.component.scss']
+  styleUrls: ['./top-contributor-by-commit.component.scss'],
 })
-export class TopContributorByCommitComponent {
+export class TopContributorByCommitComponent implements OnInit {
+  top = 10;
+  selectedRepos: {
+    owner: string;
+    name: string;
+    // contributors?: { contributorDetails: User; prCount: number }[];
+    contributors?: { login: string; commitCount: number }[];
+  }[] = [];
 
+  constructor(
+    private insightsService: InsightsService,
+    private gs: GlobalStateService
+  ) {
+    this.selectedRepos = this.gs.selectedRepos;
+  }
+  ngOnInit() {
+    this.getContributors();
+  }
+  getContributors() {
+    console.log(this.selectedRepos);
+    const endDate = new Date();
+    let startDate = new Date();
+    startDate.setMonth(endDate.getMonth() - 3);
+    this.selectedRepos.map(async (repo) => {
+      console.log(`${repo.owner}/${repo.name}`);
+
+      this.selectedRepos.map(async (repo) => {
+        repo.contributors =
+          await this.insightsService.getTopCollaboratorsByCommits(
+            repo.owner,
+            repo.name,
+            startDate,
+            endDate,
+            this.top
+          );
+        return repo;
+      });
+    });
+  }
 }
