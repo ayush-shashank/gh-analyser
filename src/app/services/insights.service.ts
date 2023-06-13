4c83edf065e0dbadeb37a6429bb6b3e32856b135
Ayush Shashank <ayush.bibhas@gmail.com>
2023-06-14 02:40:51 +0530

    Insights service fix

--- src/app/services/insights.service.ts
+++ src/app/services/insights.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GithubService } from './github.service';
+import { User } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -7,16 +8,52 @@ import { GithubService } from './github.service';
 export class InsightsService {
   constructor(private gh: GithubService) {}
 
-  getTop10CollaboratorsByCommit(owner: string, repo: string) {
-    const collaborators = this.gh.getCollaboratorContributions(owner, repo);
-    return collaborators
-      .sort((a, b) => a.commitCount - b.commitCount)
-      .slice(0, Math.min(10, collaborators.length));
-  }
+  // async getTopCollaboratorsByCommit(
+  //   owner: string,
+  //   repo: string,
+  //   count: number
+  // ) {
+  //   const collaborators = await this.gh.getCollaboratorContributions(
+  //     owner,
+  //     repo
+  //   );
+  //   return collaborators
+  //     .sort((a, b) => a.commitCount - b.commitCount)
+  //     .slice(0, Math.min(count, collaborators.length));
+  // }
 
-  getTop10CollaboratorsByPR(owner: string, repo: string) {
-    const collaborators = this.gh.getCollaboratorContributions(owner, repo);
-    return collaborators.sort((a, b) => a.prCount - b.prCount).slice(0, 10);
+  // async getTopCollaboratorsByPR(owner: string, repo: string, count: number) {
+  //   const collaborators = await this.gh.getCollaboratorContributions(
+  //     owner,
+  //     repo
+  //   );
+  //   return collaborators
+  //     .sort((a, b) => a.prCount - b.prCount)
+  //     .slice(0, Math.min(count, collaborators.length));
+  // }
+
+  async getTopCollaboratorsByPR(
+    owner: string,
+    repo: string,
+    startDate = new Date(),
+    endDate: Date,
+    count: number
+  ) {
+    const collaborators = await this.gh.getContributorByPR(
+      owner,
+      repo,
+      startDate,
+      endDate
+    );
+    let frDist: { login: string; prCount: number }[] = [];
+    let frequency =
+      this.calculateCollaboratorsFrequency(collaborators).entries();
+    for (const [key, value] of frequency) {
+      frDist.push({ login: key, prCount: value });
+    }
+    return frDist
+      .sort((a, b) => b.prCount - a.prCount)
+      .slice(0, Math.min(count, collaborators.length));
   }
 
   getCommitFrequency(
@@ -44,5 +81,40 @@ export class InsightsService {
 
   getStats(owner: string, repo: string) {
     return this.gh.getStats(owner, repo);
+  }
+
+  calculateCollaboratorsFrequency(collaborators: User[]): Map<string, number> {
+    const frequencyMap = new Map<string, number>();
+    for (const user of collaborators) {
+      const login = user.login;
+      if (frequencyMap.has(login)) {
+        frequencyMap.set(login, frequencyMap.get(login)! + 1);
+      } else {
+        frequencyMap.set(login, 1);
+      }
+    }
+    return frequencyMap;
+  }
+
+  async getTopCollaboratorsByCommits(
+    owner: string,
+    repo: string,
+    startDate: Date,
+    endDate: Date,
+    count: number
+  ) {
+    let commits = await this.gh.getRepoCommits(owner, repo, startDate, endDate);
+    const collaborators = commits
+      .filter((commit) => (commit.author.user ? true : false))
+      .map((commit) => commit.author.user!);
+    let frDist: { login: string; commitCount: number }[] = [];
+    let frequency =
+      this.calculateCollaboratorsFrequency(collaborators).entries();
+    for (const [key, value] of frequency) {
+      frDist.push({ login: key, commitCount: value });
+    }
+    return frDist
+      .sort((a, b) => b.commitCount - a.commitCount)
+      .slice(0, Math.min(count, collaborators.length));
   }
 }
